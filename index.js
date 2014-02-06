b6d1914d9ec7fdf1e35a2535c32d00bd166f0218
Thomas Leen <tl@thomasleen.com>
2014-02-06 00:22:48 +0000

    major seperation of hashed functions vs key functions, added duration based expiry

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 'use strict';
 
-var defaultStorage = require('cachy-default'),
+/*
+* Unsure the cache <-> storage relationship is well defined enough
+* Should allow delegation of tasks to storage when it is more efficient
+* but keep them available in cache for simple impl
+*/
+
+var defaultStorage = require('cachy-default');
+
+var async = require('async'),
 crypto = require('crypto'),
+moment = require('moment'),
 pkg = require('./package.json'),
 _ = require('underscore');
 
@@ -10,31 +19,123 @@ module.exports = function(store, config){
   var storage = (store || defaultStorage());
 
   var configuration = _.defaults({}, config, {
-    duration : '5 minutes'
+    duration : false
   });
 
-  var hashFunction = function(key){
+  var duration = (configuration.duration || false);
+
+  var hash = (((typeof storage.hash) === 'function') ? storage.hash : function(key){
     return crypto.createHash('md5').update(JSON.stringify(key)).digest('hex');
-  };
+  });
  
  // wrap put/get with key set
 
-  function container(key, data){
-    return JSON.stringify({
-      timestamp : Date.now(),
+  function pack(key, hashed, data){   
+    var returner = {
+      created : Date.now(),      
       key : key,
+      hash : hashed,
       data : data
-    });
+    };
+
+    if(duration){      
+      returner.expires = (returner.created + duration);
+    }
+
+//    console.log(returner);
+    return returner;
   }
 
-  function uncontainer(data){
+  function unpack(data){
     return data.data;
   }
 
-  return _.defaults({
-    hash : hashFunction
-    },storage,{
+  function putHash(hash, key, value, callback){
+    storage.write(hash, pack(key, hash, value), callback);
+  }
+
+  function put(key, value, callback){
+    putHash(hash(key), key, value, callback);
+  }
+
+  function getHash(hash, callback){
+    storage.read(hash, function(err, data){
+      if(err) return callback(err);
+      
+      if(data.expires && (data.expires <= Date.now())){
+//	console.log('expiring data:', data.key);
+	storage.remove(data.hash);
+	return callback(new Error('expired: ' + data.key));
+      }
+
+      return callback(null, unpack(data));
+    });
+  }
+
+  function get(key, callback){
+    getHash(hash(key), callback);
+  }
+
+
+  function hasHash(hash, callback){
+    getHash(hash, function(err, data){
+//      console.log('hasHash', hash, err);
+      return callback((!err));
+    });
+  }
+
+  function has(key, callback){
+    return hasHash(hash(key), callback);
+  }
+
+  function remove(key, callback){
+    storage.remove(hash(key), callback);
+  }
+  
+  function clear(callback){
+    storage.clear(callback);
+  }
+
+  // xx- allow delegation to storage
+  function size(callback){
+    storage.keys(function(err, hashes){ // storage keys are the hashes
+      if(err) return callback(err);
+      if(!duration) return callback(hashes.length);
+      
+//      console.log('size found hashes', hashes);
+      // if duration we need to check for expired entries
+
+      async.map(hashes, function(h, callback){
+	hasHash(h, function(bool){ callback(null, bool); });
+      }, function(err, results){
+//	console.log(results);
+	if(err) return callback(err);
+	var len = _.filter(results, function(bool) { return bool; }).length;
+//	console.log('duration based size returning', len);
+	return callback(null, len);		
+      });
+
+    });
+  }
+  
+ // get all keys and return formatted object representing cache state
+  function info(callback){
+    storage.keys(function(err, keys){
+      if(err) return callback(err);
+      var returner = {};    
+      return callback(null, returner);
+    });
+  }
+
+  return {
+    put : put,
+    has : has,
+    get : get,
+    remove : remove,
+    clear : clear,
+    size : size,
+    info : info,
     version : pkg.version
-  });
+  };
 
 };
--- test/index.js
+++ test/index.js
@@ -1,11 +1,12 @@
 'use strict';
 
+// xx - add timed cache tests
+
 var filesystemStore = require('cachy-filesystem'),
 pkg = require('../package.json'),
 should = require('should'),
 _ = require('underscore');
 
-// sort of stupid tests, but they are my most basic and initial tests to get testing up and going
 describe('versioning', function(){
   var cache = require('..')();
   it('should have a version', function(){
@@ -17,67 +18,161 @@ describe('versioning', function(){
   });
 });
 
-
-
-var fxns = ['write','has','read','remove','clear','keys'];
-function verifyInterface(cache){
-  describe('interface has', function(){
-    _.each(fxns, function(fxn){
-      it(fxn + '()', function(){
-	cache[fxn].should.be.a.Function;
-      });
-    });
-  });
-}
-
-// test hash
-describe('default hash', function(){
-  var cache = require('..')();
-  
-  describe('can handle input', function(){
-    it('empty string', function(){
-      cache.hash('').should.equal('9d4568c009d203ab10e33ea9953a0264');
-    });
-
-    it('numbers', function(){
-      cache.hash(12345).should.equal('827ccb0eea8a706c4c34a16891f84e7b');
-      cache.hash(77.0).should.equal('28dd2c7955ce926456240b2ff0100bde');
-    });
-
-    it('objects', function(){
-      cache.hash({foo : 'bar', baz : 'foo'}).should.equal('d4998d3e1ce20a86f073d24501fea77c');
-    });
-
-    it('handles empty object', function(){
-      cache.hash({}).should.equal('99914b932bd37a50b983c5e7c90ae93b');
-    });
-  });
-
-  it('returns same values for equal keys', function(){
-    cache.hash('foobar').should.equal(cache.hash('foobar'));
-  });
-  
-  
-});
-
 describe('default storage', function(){
   var cache = require('..')();
-  verifyInterface(cache);
 
-  _.each(fxns, function(fxn){
-    it(fxn + '() should callback an error', function(done){
-      cache[fxn](function(err){
+  describe('should return', function(){
+    it('error for put()', function(done){
+      cache.put('key', 'value', function(err){
 	err.should.be.a.Error;
 	return done();
       });
     });
+
+    it('false for has()', function(done){
+      cache.has('key', function(err){
+	err.should.be.false;
+	return done();
+      });
+    });
+
+    it('error for get()', function(done){
+      cache.get('key', function(err){
+	err.should.be.a.Error;
+	return done();
+      });
+    });
+
+    it('error for remove()', function(done){
+      cache.remove('key', function(err){
+	err.should.be.a.Error;
+	return done();
+      });
+    });
+
+    it('error for clear()', function(done){
+      cache.clear(function(err){
+	err.should.be.a.Error;
+	return done();
+      });
+    });
+
+    it('error for size()', function(done){
+      cache.size(function(err){
+	err.should.be.a.Error;
+	return done();
+      });
+    });
+
+    it('error for info()', function(done){
+      cache.info(function(err){
+	err.should.be.Error;
+	return done();
+      });
+    });
+
+
   });
 });
-/*
-describe('filesystem store backed', function(){
-  var store = filesystemStore();
-  var cache = require('..')(store);
-  verifyInterface(cache);
-});
-*/
 
+
+
+var objects = [
+  {
+    key : 'This is the key for an object',
+    data : {first: 1, second: 2, third : 'third'}
+  },
+  {
+    key: 654321,
+    data : 123456
+  }
+];
+
+_.each([
+  {
+    name : 'filesystem',
+    store : filesystemStore()
+  }], function(storage){
+    describe(storage.name + ' storage', function(){
+      var cache = require('..')(storage.store);
+      
+      describe('put()', function(){
+	_.each(objects,  function(obj){
+	  it('should put object "' + obj.key + '"', function(done){
+	    cache.put(obj.key, obj.data, function(err){
+	      should.not.exist(err);
+	      return done();
+	    });
+	  });
+	});
+      });
+
+      describe('get()', function(){
+	_.each(objects,  function(obj){
+	  it('should get object "' + obj.key + '"', function(done){
+	    cache.get(obj.key, function(err, data){
+	      should.not.exist(err);
+	      obj.data.should.eql(data);
+	      return done();
+	    });
+	  });
+	});
+      });
+
+      // has
+      describe('has()', function(){
+	_.each(objects,  function(obj){
+	  it('should have object "' + obj.key + '"', function(done){
+	    cache.has(obj.key, function(has){
+	      has.should.be.true;
+	      return done();
+	    });
+	  });
+	});
+
+	_.each(['badkey', 'another non existant key'],  function(key){
+	  it('should not have object "' + key + '"', function(done){
+	    cache.has(key, function(has){
+	      has.should.be.false;
+	      return done();
+	    });
+	  });
+	});
+      });
+      
+      describe('size()', function(){
+	it('should equal put() objects', function(done){
+	  cache.size(function(size){
+	    size.should.equal(objects.length);
+	    done();
+	  });
+	});
+      });
+
+      
+      describe('remove()', function(){
+	it('should remove the entry "' + objects[0].key + '"', function(done){
+	  cache.remove(objects[0].key, function(err){
+	    should.not.exist(err);
+	    cache.size(function(size){
+	      size.should.equal(objects.length - 1);
+	      done();
+	    });
+	  });
+	});
+      });
+      // clear
+
+      describe('clear()', function(){
+	it('should clear the cache', function(done){
+	  cache.clear(function(err){
+	    should.not.exist.err;
+	    cache.size(function(size){
+	      size.should.equal(0);
+	      done();
+	    });
+	  });
+	});
+      });
+    });
+  });
